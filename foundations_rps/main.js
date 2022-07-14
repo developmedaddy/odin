60c3fc9e5920f03403569e55f018355a017c1945
developmedaddy <developmedaddy@aliensz.org>
2022-07-13 22:49:01 -0400

    Change input to buttons and print winner text

--- foundations_rps/main.js
+++ foundations_rps/main.js
@@ -1,25 +1,31 @@
-let choices = ['r', 'p', 's'];
+let choices = ['rock', 'paper', 'scissors'];
+let buttons = document.querySelectorAll('.js-btn');
+let winnerText = document.querySelector('.js-winner');
 
-function computerPlay() {
+// Add event handler for each button
+buttons.forEach(button => button.addEventListener('click', playGame));
+
+
+function playGame(e) {
+  // Gets the button title
+  let playerChoice = e.target.previousElementSibling.textContent.toLowerCase();
+  let winner = findWinner(playerChoice, computerChoose());
+  winnerText.innerText = winner;
+}
+
+
+function computerChoose() {
   let choice = choices[Math.floor(Math.random() * 3)];
   return choice;
 }
 
-function playerPlay() {
-  let choice = '';
-  while (choice !== 'r' && choice !== 'p' && choice !== 's') {
-    choice = prompt('Choose r (rock), p (paper), or s (scissors)');
-  }
 
-  return choice;
-}
+function findWinner(playerSelection, computerSelection) {
+  let winner;
 
-function playRound(playerSelection, computerSelection) {
-  let winner = '';
-
-  if (playerSelection === 'r' && computerSelection === 's' ||
-      playerSelection === 'p' && computerSelection === 'r' ||
-      playerSelection === 's' && computerSelection === 'p') {
+  if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+      playerSelection === 'paper' && computerSelection === 'rock' ||
+      playerSelection === 'scissors' && computerSelection === 'paper') {
     winner = 'player';
   }
   else {
@@ -28,14 +34,4 @@ function playRound(playerSelection, computerSelection) {
   return winner;
 }
 
-function game(n) {
-  for (let i = 1; i <= n; i++) {
-    alert(`The winner of round ${i}/${n} is: ${playRound(playerPlay(), computerPlay())}`);
-  }
-}
-
-
-
-game(parseInt(prompt('How many games would you like to play?')));
-
-
+//function game(n) { for (let i = 1; i <= n; i++) { alert(`The winner of round ${i}/${n} is: ${playRound(playerPlay(), computerPlay())}`); } }
